6e70ac4320f813ee9cb56c67731fcd1bb2a9a6e0
akhilesh423 <akhilakhilesh423@gmail.com>
2024-07-24 13:29:51 +0530

    page setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Benefits from './components/Benefits';
-import Footer from './components/Footer';
-
+import './pages/LandingPage'
+import LandingPage from './pages/LandingPage';
 const App: React.FC = () => {
     return (
         <>
-     <Navbar/>
-    <Hero/>
-    <Benefits/>
-    <Footer/>
+        <LandingPage/>
         </>
           
        
---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero';
+import Navbar from '../components/Navbar';
+import Benefits from '../components/Benefits';
+import Footer from '../components/Footer';
+
+
+
+export default function LandingPage(){
+    return(
+        <>
+         <Navbar/>
+        <Hero/>
+        <Benefits/>
+         <Footer/>
+        </>
+    )
+}
