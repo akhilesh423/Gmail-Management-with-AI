e1987e1fa75a4f205c3a7f0a60236f94067a87e5
akhilesh423 <akhilakhilesh423@gmail.com>
2024-07-26 10:42:38 +0530

    deploy

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,11 +34,7 @@ const oauth2callback = async (req, res) => {
   try {
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
-    res.cookie('id_token', tokens.id_token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'Lax',
-    });
+    res.cookie('id_token', tokens.id_token);
   
     res.redirect('http://localhost:5173/dashboard');
   } catch (error) {
